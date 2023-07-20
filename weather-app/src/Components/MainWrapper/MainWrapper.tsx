20dc1e02a39a918e3cbcfc77b77396a8bb259ca6
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-07-20 19:49:40 +0300

    add component MainWrapper

---
+++ weather-app/src/Components/MainWrapper/MainWrapper.tsx
@@ -0,0 +1,17 @@
+import './MainWrapper.css';
+import CitiesList from "../../Components/CitiesList/CitiesList";
+import MainHeading from "../../Components/MainHeading/MainHeading";
+import CityItem from "../../Components/CityItem/CityItem";
+
+
+const MainWrapper = () => {
+  return (
+    <div>
+        <MainHeading/>
+        <CitiesList/>
+        <CityItem/>
+    </div>
+  )
+}
+
+export default MainWrapper
