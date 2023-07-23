20185e861905b636f3246013a8d88bbbef29bd02
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-07-23 13:31:01 +0300

    change working logic so that searched city data displays

--- weather-app/src/Components/CitiesList/CitiesList_Simona.js
+++ weather-app/src/Components/CitiesList/CitiesList_Simona.js
@@ -1,8 +1,10 @@
 import "./CitiesList.css";
+import '../CityItem/CityItem.css';
 import  Cities from "../../DataBase/data.js";
 import { useState } from "react";
-import Paginator from "../Paginator/Paginator";
-import SearchBar from "../Search/Search1";
+import CityItemCard from "../CityItemCard/CityItemCard";
+// import Paginator from "../Paginator/Paginator";
+// import SearchBar from "../Search/Search1";
 
 
 
@@ -14,15 +16,14 @@ const CitiesList = () => {
     
     const [filterSearch, setFilterSearch] = useState("");
     const filterBySearch = (e) => {
-        setFilterSearch(e.target.value);
-console.log(filterSearch);
-
-      
+      setFilterSearch(e.target.value);
+      console.log(filterSearch);
     };
 
     const handleEnter = (e) => {
       if (e.key === "Enter") {
         const filtered = cities.filter(c => c.cityName.toLowerCase().includes(filterSearch));
+        console.log(filtered);
         setCities(filtered);
       }
 
@@ -36,26 +37,64 @@ console.log(filterSearch);
     
 
   return (
-      <div>
-            <div className="citiesListTableWrapper">
+    <div>
+      <div className="citiesListTableWrapper">
         <div className="SearchInput">
-            <input type="text" className="form-control" value={filterSearch} onChange={filterBySearch} onKeyUp ={handleEnter} placeholder="Search..." />
+          <input 
+            type="text" 
+            className="form-control" 
+            value={filterSearch} 
+            onChange={filterBySearch} 
+            onKeyUp ={handleEnter} 
+            placeholder="Search..." 
+          />
         </div>
-        <div>
-            {cities.map(i =>
-                <div key={i.cityName}>
-                    {i.cityName}
-                </div>
-            )};
-      </div>
-          
+        {/* <div>
+              {cities.map(i =>
+                  <div key={i.cityName}>
+                      {i.cityName}
+                  </div>
+              )};
+        </div> */}
+
+        {/* CityItem component --> */}
+
+        {cities.map((i) => {
+          const FilteredData = Object.values(i.averageTemperatureCelsius);
+          // const monthNamesData = FilteredData.map(
+          //   (month) => i.averageTemperatureCelsius[month].monthName
+          // );
+          // const tempCelsiusData = FilteredData.map(
+          //   (month) => i.averageTemperatureCelsius[month].tempCelsius
+          // );
+          console.log(i.averageTemperatureCelsius);
+
+          return (
+            <div key={i.cityName} className="CityItem">
+              <div className="CityItemCityName">{i.cityName}</div>
+              <div className="CityItemCardWrapper">
+                {FilteredData.map((data) => (
+                  <CityItemCard 
+                    month={data.monthName}  
+                    tempCelsius={data.tempCelsius}
+                  />
+                ))}
+              </div>
+              {/* <CityItemCard
+                monthNamesData={monthNamesData}
+                tempCelsiusData={tempCelsiusData}
+              /> */}
+            </div>
+          );
+        })}
+
+        
+        {/* <-- CityItem component */}
       </div>
 
-      </div>
-        );
+    </div>
+  );
   
 };
-
-//TODO place component within page and page within App.tsx
 
 export default CitiesList;
