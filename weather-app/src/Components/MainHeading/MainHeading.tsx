dcc156fdeff8ba177adf7e8090046e31a8aacf44
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-07-24 12:27:57 +0300

    improved css

--- weather-app/src/Components/MainHeading/MainHeading.tsx
+++ weather-app/src/Components/MainHeading/MainHeading.tsx
@@ -5,11 +5,11 @@ const MainHeading = () => {
     <div className='MainHeadingWrapper'>
         <div className="MainHeadingIcons">
             
-            <img width="50" height="50" src="https://img.icons8.com/ios/50/rain--v1.png" alt="rain--v1"/>
-            <img width="50" height="50" src="https://img.icons8.com/ios/50/snow.png" alt="snow"/>            
-            <img width="50" height="50" src="https://img.icons8.com/ios/50/partly-cloudy-day--v1.png" alt="partly-cloudy-day--v1"/>
-            <img width="50" height="50" src="https://img.icons8.com/ios/50/sun--v1.png" alt="sun--v1"/>
-            <img width="50" height="50" src="https://img.icons8.com/ios/50/cloud--v1.png" alt="cloud--v1"/>
+            <img src="https://img.icons8.com/ios/50/rain--v1.png" alt="rain--v1"/>
+            <img src="https://img.icons8.com/ios/50/snow.png" alt="snow"/>            
+            <img src="https://img.icons8.com/ios/50/partly-cloudy-day--v1.png" alt="partly-cloudy-day--v1"/>
+            <img src="https://img.icons8.com/ios/50/sun--v1.png" alt="sun--v1"/>
+            <img src="https://img.icons8.com/ios/50/cloud--v1.png" alt="cloud--v1"/>
             
         </div>
         <h1 className="MainHeading">
